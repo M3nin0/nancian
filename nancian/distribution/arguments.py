70fd728f15b593d4efe00336673ee71ed5f66786
alexNeto <alex.neto3@hotmail.com>
2018-05-27 13:14:00 -0300

    criado distribuição binomial

---
+++ nancian/distribution/arguments.py
@@ -0,0 +1,24 @@
+import math
+
+
+class Binomial:
+    q = None
+    p = None
+    k = None
+    n = None
+    PXk = None
+
+    def __init__(self):
+        """
+        PXk -> P(X = k) - probabilidade de que o evento se realize “k vezes” em “n provas”
+        p - probabilidade de sucesso
+        """
+        pass
+
+class Poisson:
+    Px = None
+    e = math.e
+    lamb = None
+    mi = None
+    t = None
+    X = None
---
+++ nancian/distribution/binomial_distribution.py
@@ -0,0 +1,22 @@
+import math
+
+
+class BinomialDistribution:
+    def __init__(self, arguments):
+        self.arguments = arguments
+        self.nk = self.arguments.n - self.arguments.k
+
+    def calculaIgual(self):
+        self.arguments.PXk = self._a() / self._b() * self._c()
+        return self.arguments
+
+    def _a(self):
+        return math.factorial(self.arguments.n)
+
+    def _b(self):
+        return math.factorial(self.arguments.k) * \
+               math.factorial(self.nk)
+
+    def _c(self):
+        return math.pow(self.arguments.p, self.arguments.k) * \
+               math.pow(self.arguments.q, self.nk)
