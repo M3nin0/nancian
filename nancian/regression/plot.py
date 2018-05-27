3635d101cdb80cf1bc748e34e0f6f71f1f63c4df
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2018-05-27 16:56:35 -0300

    Adição do notebook de explicação de uso da função de regressão linear, e melhoria no plot da reta

--- nancian/regression/plot.py
+++ nancian/regression/plot.py
@@ -2,13 +2,18 @@
 
 import matplotlib.pyplot as plt
 
-def plot_disp(x: list, y: list, plot_title: str):
+def plot_disp(x: list, y: list, plot_title: str, fit_function):
     """Função para criar diagrama de dispersão
     :param: x: Lista com os valores em X
     :param: y: Lista com os valores em Y
     :param: plot_title: Titulo do plot
+    :param: fit_function: Função para realização do fit da linha
     """
+
+    # Preparando a linha de fit
+    xfit, yfit = (min(x), max(x)), (fit_function(min(x)), fit_function(max(x)))
     
-    plt.figure
+    plt.figure()
     plt.title(plot_title)
+    plt.plot(xfit, yfit, 'b')
     plt.plot(x, y, marker='o', linestyle='')
