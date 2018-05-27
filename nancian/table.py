31e9a5092d2ef0cdd3576792a48ec714b996605f
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2018-05-27 19:27:25 -0300

    Atualização da busca nas tabelas de distribuição

--- nancian/table.py
+++ nancian/table.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 
+
 def table_five(x: list, y: list):
     """Função para geração da tabela utilizada na lista 5 (Pacote five)
     :param: x: Valores em X
@@ -30,23 +31,49 @@ def table_five(x: list, y: list):
     
     return table
 
-def nearest_neighbors_norm(element: float):
+
+def nearest_neighbors(element: float, type_dist: str , typeof: str = None):
     """Função para buscar o elemento mais próximo de um dado elemento
     :param: element: Elemento referência na busca de proximidade
-    :param: table: Tabela onde a busca será realizada
+    :param: type_dist: Tipo de distribuição
+        - normal;
+        - student
+    :param: typeof: Tipo de teste que está sendo realizado
+        - monocaudal;
+        - bicaudal.
     
     Para encontrar o elemento mais próximo será utilizado a distância simples
     d = Eq - Ep
     """
     
-    table = pd.read_csv('nancian/tables/normal_dist_table.csv')
+    if type_dist == 'normal':
+        table = pd.read_csv('nancian/tables/normal_dist_table.csv')
+        
+        # Mapeia as colunas da tabela
+        # Aqui a primeira está sendo desconsiderada como coluna de referência
+        columns = table.columns[1:]
+        
+        # Definindo qual a diferença entre a tabela e o index
+        off_set = 2
     
+    elif type_dist == 'student':
+        full_table = pd.read_csv('nancian/tables/student_dist_table.csv', header = None)
+        table = full_table.iloc[2:]
+    
+        # Recriando a tabela de acordo com o tipo de teste
+        if typeof == 'monocaudal':
+            table.columns = list(full_table.iloc[0:1:, 0:].values[0])
+        elif typeof == 'bicaudal':
+            table.columns = list(full_table.iloc[1:2:, 0:].values[0])
+
+        # Aqui a primeira está sendo desconsiderada como coluna de referência
+        columns = table.columns[1:]
+
+        # Definindo qual a diferença entre a tabela e o index
+        off_set = 3
+
     # Armazenamento das informações do elemento encontrado
     neighbor = {'neighbor': 0, 'column': 0, 'row': 0, 'distance': 1} 
-
-    # Mapeia as colunas da tabela
-    # Aqui a primeira está sendo desconsiderada como coluna de referência
-    columns = table.columns[1:]
     
     for column in columns:
         row = 0
@@ -55,6 +82,6 @@ def nearest_neighbors_norm(element: float):
                 neighbor['distance'] = abs(item - element)
                 neighbor['neighbor'] = item
                 neighbor['column'] = column
-                neighbor['row'] = row + 2
+                neighbor['row'] = row + off_set
             row += 1
     return neighbor
