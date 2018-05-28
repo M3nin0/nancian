5232f57b252a8a97ee9697a3e8fb17485601fcb6
alexNeto <alex.neto3@hotmail.com>
2018-05-27 22:01:20 -0300

    adicionado calculos para nenor e maior que

--- nancian/distribution/binomial_distribution.py
+++ nancian/distribution/binomial_distribution.py
@@ -10,6 +10,32 @@ class BinomialDistribution:
         self.arguments.PXk = self._a() / self._b() * self._c()
         return self.arguments
 
+    def calculaMenorQue(self, inicio, fim):
+        result = 0
+        temp = None
+        while inicio < fim:
+            self.arguments.k = inicio
+            result += self.calcula().PXk
+            inicio += 1
+        self.arguments.PXk = result
+        return self.arguments
+
+    def calculaMenorIgualQue(self, inicio, fim):
+        return self.calculaMenorQue(inicio, fim + 1)
+
+    def calculaMaiorQue(self, inicio, offset=0):
+        result = 0
+        fim = self.arguments.n + offset
+        while inicio < fim:
+            self.arguments.k = inicio
+            result += self.calcula().PXk
+            inicio += 1
+        self.arguments.PXk = result
+        return self.arguments
+
+    def calculaMaiorIgualQue(self, inicio):
+        return self.calculaMaiorQue(inicio, 1)
+
     def _a(self):
         return math.factorial(self.arguments.n)
 
@@ -20,3 +46,4 @@ class BinomialDistribution:
     def _c(self):
         return math.pow(self.arguments.p, self.arguments.k) * \
                math.pow(self.arguments.q, self.nk)
+
